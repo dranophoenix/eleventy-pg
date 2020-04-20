b91a393dbe2569886a5b11f26e7892db6fc82f7b
unknown <dog_champ@hotmail.com>
2020-04-20 12:08:49 +0700

    remove import graphql. we will try on branch. let master to be init proj

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,5 @@
-var { graphql, buildSchema } = require("graphql");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("filesize", function (path) {
     return "0KBB";
   });
-
-  // this could also be `async function`
-  // if you want to `await` for other things here, use `async function`
-  var schema = buildSchema(`type Query {
-    hello: String
-  }`);
-
-  var root = {
-    hello: () => "Hello world async!",
-  };
-
-  return graphql(schema, "{ hello }", root);
 };
